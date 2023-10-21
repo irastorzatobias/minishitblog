8b664566fc6bacd378fe3e16d51aa8f74e1bef28
tobias <tobirastorza@gmail.com>
2023-10-21 09:37:03 -0300

    urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,14 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import EntradaListView, EntradaCreateView, EntradaDeleteView, EntradaUpdateView, ComentarioCreateView, ComentarioDeleteView
+from .api import CategoriaViewSet, EntradaViewSet, ComentarioViewSet, CustomUserViewSet
+
+router = DefaultRouter()
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'entradas', EntradaViewSet)
+router.register(r'comentarios', ComentarioViewSet)
+router.register(r'usuarios', CustomUserViewSet)
 
 urlpatterns = [
     path('', EntradaListView.as_view(), name='entrada_list'),
     path('entrada/new/', EntradaCreateView.as_view(), name='entrada_new'),
-    path('entrada/<int:pk>/delete/',
-         EntradaDeleteView.as_view(), name='entrada_delete'),
+    path('entrada/<int:pk>/delete/', EntradaDeleteView.as_view(), name='entrada_delete'),
     path('entrada/<int:pk>/edit/', EntradaUpdateView.as_view(), name='entrada_edit'),
-    path('entrada/<int:pk>/comentario/new/',
-         ComentarioCreateView.as_view(), name='comentario_new'),
-    path('comentario/<int:pk>/delete/',
-         ComentarioDeleteView.as_view(), name='comentario_delete'),
+    path('entrada/<int:pk>/comentario/new/', ComentarioCreateView.as_view(), name='comentario_new'),
+    path('comentario/<int:pk>/delete/', ComentarioDeleteView.as_view(), name='comentario_delete'),
+    path('api/', include(router.urls)),
 ]
