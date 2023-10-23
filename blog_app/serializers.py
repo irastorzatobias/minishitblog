bc215fe55133c52ec964bc133da564fa9758f20d
tobias <tobirastorza@gmail.com>
2023-10-23 01:46:40 -0300

    fixed user post problem

--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -20,3 +20,10 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
