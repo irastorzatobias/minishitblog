628af3727f5e4813e7513ce9bca96f16a634906f
SebaMarzari <sebamarzari98@gmail.com>
2023-10-26 02:01:22 -0300

    BaseViewSet for Entradas && Comentarios

--- blog_app/api.py
+++ blog_app/api.py
@@ -1,8 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .models import Categoria, Entrada, Comentario, CustomUser
 from .serializers import CategoriaSerializer, EntradaSerializer, ComentarioSerializer, CustomUserSerializer
-from rest_framework import permissions, status
-from rest_framework.response import Response
+
+class BaseViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.autor != request.user:
+            return Response({"detail": "No tienes permiso para eliminar este objeto."}, status=status.HTTP_403_FORBIDDEN)
+        self.perform_destroy(instance)
+        return Response({"detail": "El objeto se eliminó con éxito."}, status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.autor != request.user:
+            return Response({"detail": "No tienes permiso para actualizar este objeto."}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
@@ -10,51 +25,15 @@ class CategoriaViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-class EntradaViewSet(viewsets.ModelViewSet):
+class EntradaViewSet(BaseViewSet):
     queryset = Entrada.objects.all()
     serializer_class = EntradaSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        if instance.autor != request.user:
-            return Response({"detail": "No tienes permiso para eliminar esta entrada."}, status=status.HTTP_403_FORBIDDEN)
-
-        self.perform_destroy(instance)
-        return Response({"detail": "La entrada se eliminó con éxito."}, status=status.HTTP_204_NO_CONTENT)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        if instance.autor != request.user:
-            return Response({"detail": "No tienes permiso para actualizar esta entrada."}, status=status.HTTP_403_FORBIDDEN)
-
-        return super().update(request, *args, **kwargs)
-
-class ComentarioViewSet(viewsets.ModelViewSet):
+class ComentarioViewSet(BaseViewSet):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        if instance.autor != request.user:
-            return Response({"detail": "No tienes permiso para eliminar este comentario."}, status=status.HTTP_403_FORBIDDEN)
-
-        self.perform_destroy(instance)
-        return Response({"detail": "El comentario se eliminó con éxito."}, status=status.HTTP_204_NO_CONTENT)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        if instance.autor != request.user:
-            return Response({"detail": "No tienes permiso para actualizar este comentario."}, status=status.HTTP_403_FORBIDDEN)
-
-        return super().update(request, *args, **kwargs)
-
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
