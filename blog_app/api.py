81bb3ac7a654fe1b2683f38de8dadf1223109b0c
tobias <tobirastorza@gmail.com>
2023-10-25 23:40:29 -0300

    finished without doc && tests

--- blog_app/api.py
+++ blog_app/api.py
@@ -24,10 +24,36 @@ class EntradaViewSet(viewsets.ModelViewSet):
         self.perform_destroy(instance)
         return Response({"detail": "La entrada se eliminó con éxito."}, status=status.HTTP_204_NO_CONTENT)
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.autor != request.user:
+            return Response({"detail": "No tienes permiso para actualizar esta entrada."}, status=status.HTTP_403_FORBIDDEN)
+
+        return super().update(request, *args, **kwargs)
+
 class ComentarioViewSet(viewsets.ModelViewSet):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.autor != request.user:
+            return Response({"detail": "No tienes permiso para eliminar este comentario."}, status=status.HTTP_403_FORBIDDEN)
+
+        self.perform_destroy(instance)
+        return Response({"detail": "El comentario se eliminó con éxito."}, status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.autor != request.user:
+            return Response({"detail": "No tienes permiso para actualizar este comentario."}, status=status.HTTP_403_FORBIDDEN)
+
+        return super().update(request, *args, **kwargs)
+
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
